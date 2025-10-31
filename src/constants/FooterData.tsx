cd937bfcaf966e4abc975f6cc752447d893f2ba9
Mikey <kerbydalan076@gmail.com>
2025-10-31 18:40:23 +0800

    Add: Footer Data for architecture

---
+++ src/constants/FooterData.tsx
@@ -0,0 +1,17 @@
+const quickLinks = [
+  { name: 'Home', href: '#home' },
+  { name: 'Services', href: '#services' },
+  { name: 'Pricing', href: '#pricing' },
+  { name: 'FAQ', href: '#faq' },
+  { name: 'Contact', href: '#contact' },
+];
+
+const services = [
+  'Wash & Fold',
+  'Dry Cleaning',
+  'Ironing Service',
+  'Self-Service',
+  'Pickup & Delivery',
+];
+
+export { quickLinks, services };
